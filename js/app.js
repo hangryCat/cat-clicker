e9a6cb43ae13c06b24d4f200f2dd13352bc71d06
Meo Hu <mayoh727@gmail.com>
2018-09-14 14:33:00 -0700

    Reorganize code

--- js/app.js
+++ js/app.js
@@ -1,25 +1,5 @@
-// const catElement1 = document.querySelector('h1#cat-name1');
-// const catElement2 = document.querySelector('h1#cat-name2');
-//
-// const clickDiv = document.getElementsByClassName('clickDiv');
-//
-// const clickValue = document.querySelector('#clickValue');
-//
-// const catName1 = 'Dormammu';
-// const catName2 = 'Jake';
-//
-// catElement1.innerText = catName1;
-// catElement2.innerText = catName2;
-//
-// for (var i = 0; i < clickDiv.length; i++) {
-//   clickDiv[i].addEventListener('click', function() {
-//   console.log("click");
-//   clickValue.innerText++;
-//   });
-// };
-
 // Second Requirements - v2
-const container = document.querySelector('.container');
+const container = document.querySelector('#container');
 
 const catsArray = [
   {name: 'Dormammu', image: 'images/757904009_521561154_l.jpg'},
@@ -28,26 +8,30 @@ const catsArray = [
   {name: 'Kuro', image: 'images/hamiltoncatqmode.jpg'},
   {name: 'Tama', image: 'images/maxresdefault.jpg'},
   {name: 'Unko', image: 'images/Top-plus-beaux-chats-9.jpg'}
-]
+];
 
 for (var i = 0; i < catsArray.length; i++) {
-  const catDiv = document.createElement('div');
-  catDiv.classList.add('clickable');
-  container.appendChild(catDiv);
+  const catInfo = document.createElement('section');
+  catInfo.classList.add('cat-info');
+  container.appendChild(catInfo);
 
   const catName = document.createElement('h1');
   catName.innerText = catsArray[i].name;
+  catInfo.appendChild(catName);
+
+  const catDiv = document.createElement('div');
+  catDiv.classList.add('clickable');
+  catInfo.appendChild(catDiv);
 
   const catImg = document.createElement('img');
   catImg.setAttribute('src', catsArray[i].image);
+  catDiv.appendChild(catImg);
 
   const catClicks = document.createElement('h1');
+  catInfo.appendChild(catClicks);
+
   const catValue = document.createElement('h1');
   catClicks.innerText = "Number of Clicks:"
   catValue.innerText = 0;
-
-  catDiv.appendChild(catName);
-  catDiv.appendChild(catImg);
-  catDiv.appendChild(catClicks);
-  catDiv.appendChild(catValue);
-}
+  catInfo.appendChild(catValue);
+};
