df4a2634c7c88abe53c8f268447c3b6400acc93b
Meo Hu <mayoh727@gmail.com>
2018-09-13 18:27:31 -0700

    Assign variable to name elements

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
-const catName1 = "Jake";
-const catName2 = "Tomas";
+const nameElement1 = document.querySelector('h1#cat-name1');
+const nameElement2 = document.querySelector('h1#cat-name2');
+
+const catName1 = 'Jake';
+const catName2 = 'Dormammu';
