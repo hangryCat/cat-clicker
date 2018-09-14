6c2b727ad64673a03f81797cb16cfc63e5403310
Meo Hu <mayoh727@gmail.com>
2018-09-13 18:39:46 -0700

    Add cats' name using js dom

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-const nameElement1 = document.querySelector('h1#cat-name1');
-const nameElement2 = document.querySelector('h1#cat-name2');
+const catElement1 = document.querySelector('h1#cat-name1');
+const catElement2 = document.querySelector('h1#cat-name2');
 
-const catName1 = 'Jake';
-const catName2 = 'Dormammu';
+const catName1 = 'Dormammu';
+const catName2 = 'Jake';
+
+catElement1.innerText = catName1;
+catElement2.innerText = catName2;
