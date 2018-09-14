96b79c13fb63b9dc717ea7aebed317e8b6542e83
Meo Hu <mayoh727@gmail.com>
2018-09-14 12:33:55 -0700

    Start change for v2

--- js/app.js
+++ js/app.js
@@ -1,19 +1,52 @@
-const catElement1 = document.querySelector('h1#cat-name1');
-const catElement2 = document.querySelector('h1#cat-name2');
+// const catElement1 = document.querySelector('h1#cat-name1');
+// const catElement2 = document.querySelector('h1#cat-name2');
+//
+// const clickDiv = document.getElementsByClassName('clickDiv');
+//
+// const clickValue = document.querySelector('#clickValue');
+//
+// const catName1 = 'Dormammu';
+// const catName2 = 'Jake';
+//
+// catElement1.innerText = catName1;
+// catElement2.innerText = catName2;
+//
+// for (var i = 0; i < clickDiv.length; i++) {
+//   clickDiv[i].addEventListener('click', function() {
+//   console.log("click");
+//   clickValue.innerText++;
+//   });
+// };
 
-const clickDiv = document.getElementsByClassName('clickDiv');
+// Second Requirements - v2
+const container = document.querySelector('.container');
 
-const clickValue = document.querySelector('#clickValue');
+const catsArray = [
+  {name: 'Dormammu', src: 'images\757904009_521561154_l.jpg'},
+  {name: 'Jake', src: 'images\cat.jpg'},
+  {name: 'Tomas', src: 'images\cute-cat-pictures-fluffy-cat-licking-its-paws.jpg'},
+  {name: 'Kuro', src: 'images\hamiltoncatqmode.jpg'},
+  {name: 'Tama', src: 'images\maxresdefault.jpg'},
+  {name: 'Unko', src: 'images\Top-plus-beaux-chats-9.jpg'}
+]
 
-const catName1 = 'Dormammu';
-const catName2 = 'Jake';
+for (var i = 0; i < catsArray.length; i++) {
+  const catDiv = document.createElement('div');
+  catDiv.classList.add('clickable');
+  container.appendChild(catDiv);
 
-catElement1.innerText = catName1;
-catElement2.innerText = catName2;
+  const catName = document.createElement('h1');
+  catName.innerText = catsArray[i].name;
 
-for (var i = 0; i < clickDiv.length; i++) {
-  clickDiv[i].addEventListener('click', function() {
-  console.log("click");
-  clickValue.innerText++;
-  });
-};
+  const catImg = document.createElement('img');
+
+  const catClicks = document.createElement('h1');
+  const catValue = document.createElement('h1');
+  catClicks.innerText = "Number of Clicks:"
+  catValue.innerText = 0;
+
+  catDiv.appendChild(catName);
+  catDiv.appendChild(catImg);
+  catDiv.appendChild(catClicks);
+  catDiv.appendChild(catValue);
+}
